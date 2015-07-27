f2d6c4409bb960ceb491ce48e533fbf7876b0bb7
icaro <ikaro.d@live.com>
2015-07-27 12:14:18 -0400

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,40 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+//= require turbolinks
+//= require owl.carousel.js
+//= require_tree .
+
+$(function(){ $(document).foundation(); });
+
+$(document).ready(function() {
+
+	$("#slider").owlCarousel({
+
+		navigation: true, // Show next and prev buttons
+		slideSpeed: 300,
+		paginationSpeed: 400,
+		singleItem: true
+
+		// "singleItem:true" is a shortcut for:
+		// items : 1, 
+		// itemsDesktop : false,
+		// itemsDesktopSmall : false,
+		// itemsTablet: false,
+		// itemsMobile : false
+
+	});
+
+});
